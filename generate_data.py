7ccde92bb454255713f6c9675a82de20cd8d5f03
Diego Sona <diego.sona@gmail.com>
2011-11-25 18:02:46 +0100

    corrected data generator

--- generate_data.py
+++ generate_data.py
@@ -1,9 +1,10 @@
 """
-	Generate datasets with input data X and outputs Y.
-    	The target is generated with a uniform distribution in [-1, 1].
-	The first 20 features are generated from the target adding
-	incremental white Gaussian noise.  The others are generated summing
-	some white Gausian noise to a uniform distribution similar to target.
+	Generate datasets with input data X and outputs Y.  The target
+    	is generated with a uniform distribution in [-1, 1].  The
+    	first 20 features are generated from the target adding
+    	incremental white Gaussian noise.  The others are generated
+    	summing some white Gausian noise to a uniform distribution
+    	similar but independent from target .
 
 	PARAMETERS:
 	       samples    - Number of examples.
@@ -19,46 +20,48 @@
 """
 
 import numpy as np
-import math
 
 def gen_data(samples, features, impFeat, min_sigma=0.1, max_sigma=3, sigma_const=0.1):
 
-	""" The output matrix (samples, features) """
-  	X = np.empty([samples, features], float)	
+	# The output matrix (samples, features)
+  	X = np.empty((samples, features), float)	
 
-	""" Generation of dependant variable with uniform distribution in [-1,1]"""
-	
+	# Generation of dependant variable with uniform distribution in [-1,1]	
 	Y = np.random.uniform(-1,1,samples)
 	Y = Y.reshape(1,samples)
 
-	""" 
-	    Generation of independant variables adding white noise to
-	    uniform ditribution in [0,1].  Related features are equal to
-	    dependant variable with incremental white noise
-        """	
+	# Generation of independant variables adding white noise to
+	# uniform ditribution in [0,1].  Related features are equal to
+	# dependant variable with incremental white noise.
 	sigma = min_sigma
 	delta_sigma = (max_sigma - min_sigma)/impFeat
 	
-	for i in range(1, impFeat):
-		y = np.random.randn(samples, 1)
-		y = y.reshape(1,samples)
-		X[:,i] = Y + y * math.sqrt(sigma)
+	for i in range(0, impFeat):
+		X[:,i] = Y + np.random.randn(samples) * np.sqrt(sigma)
 		sigma += delta_sigma	
 	
-	for i in range(impFeat+1, features):
-		val = np.random.uniform(-1,1,samples) + np.random.randn(samples, 1) * math.sqrt(sigma_const)
-		X = np.column_stack([X, val])
+	for i in range(impFeat, features):
+		X[:,i] = np.random.uniform(-1,1,samples) + np.random.randn(samples) * np.sqrt(sigma_const)
 
-	""" Normalize the input to null mean and unitary standard deviation """	
-	meanX = np.mean(X) 
-	stdX = np.std(X)
-	if stdX != 0:
-		X = (X - meanX)/stdX 		
+	# Normalize the input to null mean and unitary standard deviation.
+	meanX = np.mean(X, axis=0) 
+	stdX = np.std(X, axis=0)
+	#if stdX != 0:         # if it is = 0 there is a mistake somewhere
+	X = (X - meanX)/stdX 		
 
 	return Y, X
 	
 	
 
+######################################################################
 if __name__ == "__main__":
-	print gen_data(100,500, 20)
 
+	# funcion testing: generate the data and test the features
+	# correlation with the dependant variable
+	y,x = gen_data(100,500, 20)
+
+	corrvalues = np.zeros(x.shape[1])
+	for i in range(x.shape[1]):
+		corrvalues[i] = np.corrcoef(y, x[:,i])[0,1]
+		pass
+	print corrvalues
